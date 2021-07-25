4d6c4a889cdeab740adc4c8382ed794f0dfced1d
Valentin Vasiliu <vali15tnt51v@gmail.com>
2021-07-25 20:38:52 +0200

    Finished project 15-Cocktails

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia
+        aliquam eos tenetur odio nihil numquam officiis quos ad optio aspernatur
+        ullam expedita ab provident laborum blanditiis culpa sint qui
+        distinctio, voluptates dicta ducimus eius error corporis similique.
+        Provident, sequi soluta!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
